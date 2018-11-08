25dd75466a6928c455acd2911cf3795f55c7033c
wulian <lian.wu@hoomsun.com>
2018-11-08 17:19:52 +0800

    Merge branch 'master' of C:\Users\hxb001\PycharmProjects\Childhood-Said with conflicts.

--- boards/models.py
+++ boards/models.py
@@ -12,6 +12,7 @@ class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
 
+
     def __str__(self):
         return self.name
 
